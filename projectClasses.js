5e950ce63abffa3f8beef21fdb0a550c8b828170
sulakshanadeore <36725147+sulakshanadeore@users.noreply.github.com>
2026-01-05 11:15:20 +0530

    Add files via upload

---
+++ projectClasses.js
@@ -0,0 +1,139 @@
+
+
+class Payment
+{
+    pay(amount)
+    {
+        console.log("Processing payment for Rs. " + amount);
+    }
+}
+
+class CardPayment extends Payment
+{
+
+
+    pay(amount)
+    {
+     super.pay(amount);
+        console.log("Paid Rs." + amount + " by card");
+    }
+}
+
+class Account{
+    #acctype="";
+    #acno=0;
+#balance=0; //private field/variable(#)
+
+
+constructor(accno,bal,accounttype)
+{
+    this.accountNo=accno;
+    this.accountbal=bal;
+   
+    this.AccountType=accounttype;
+}
+
+set accountbal(value){
+    this.#balance=value;
+}
+
+get accountbal()
+{
+    return this.#balance;
+}
+
+get AccountType()
+{
+
+    return this.#acctype;
+}
+set AccountType(value)
+{
+    this.#acctype=value;
+}
+
+set accountNo(value)
+{
+    this.#acno=value;
+
+}
+
+get accountNo()
+{
+    return this.#acno;
+}
+
+
+deposit(amount)
+{
+    if(amount>0)
+    this.#balance=this.#balance+ amount;
+console.log("Deposited Rs. "  + amount + "to " + this.#acctype);
+}
+showBalance(){
+    console.log("AccountNo=  " + this.accountNo)
+    console.log("Balance=  " + this.accountbal)
+    console.log("type=  " + this.AccountType)
+}
+
+}
+
+class Customer
+{
+    #custid;
+    #custname;
+    #cust_actype;
+    #account;
+    #accno;
+    
+
+    // constructor(custid,custname,custactype)
+    // {
+    //     this.#custid=custid;
+    //     this.#custname=custname;
+    //     this.#cust_actype=new Account(custactype)
+    // }
+
+    constructor(custid,custname,account)
+    {
+       this.#custid=custid;
+         this.#custname=custname;
+         this.#account=account;
+    }
+    showAllCustDetails()
+    {
+
+        console.log(this.#custid);
+        console.log(this.#custname);
+        console.log(this.#account);
+
+    
+       
+    
+      
+    }
+
+   
+    
+    get Custid()
+    {
+        return this.#custid;
+    }
+    set Custid(value)
+    {
+        if(value>0)
+        this.#custid=value;
+    }
+
+
+    get CustName()
+    {
+return this.#custname;
+
+    }
+
+    set CustName(value)
+    {
+        this.#custname=value;
+    }
+}
